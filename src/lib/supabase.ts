52c5d94aa1d61c19026c51dac8ddabd00a074c95
Shaun Regenbaum <shaunie@Shauns-MacBook-Pro.local>
2023-11-01 15:48:28 +0200

    sample session

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 import {PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY} from '$env/static/public';
-import type { Supabase } from './types';
+import type { Supabase, Session } from './types';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+
+export async function saveSession(session:Session){
+    const { data, error } = await supabase
+        .from('session_details')
+        .insert(session)
+        return {data, error}
+        
+}
--- src/routes/session/+page.ts
+++ src/routes/session/+page.ts
@@ -1,11 +1,12 @@
 import type { PageLoad } from './$types';
 import {getMishna} from '$lib/sefaria';
+import type { Session } from '$lib/types';
+import { saveSession } from '$lib/supabase';
 
 export const load = (async ({url}) => {
     // get session_id from url
     const session_id = url.searchParams.get('session');
-    // 
-
+    
 
     // get user_id
     // what mishna was user on
